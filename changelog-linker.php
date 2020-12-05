8ba29794341514a1f89678aedb477420e758187a
zingimmick <zingimmick@outlook.com>
2020-12-05 23:41:29 +0800

    Use constant instead of string

--- changelog-linker.php
+++ changelog-linker.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ChangelogLinker\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set('authors_to_ignore', ['zingimmick', 'dependabot[bot]', 'renovate[bot]']);
+    $parameters->set(Option::AUTHORS_TO_IGNORE, ['zingimmick', 'dependabot[bot]', 'renovate[bot]']);
 };
