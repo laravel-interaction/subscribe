cb7b61e5376425e7422899cf543d32b3c06237cc
zingimmick <zingimmick@outlook.com>
2020-12-26 22:29:16 +0800

    Fix code style

--- rector.php
+++ rector.php
@@ -2,11 +2,21 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Core\Configuration\Option;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\If_\ChangeOrIfReturnToEarlyReturnRector;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
+use Rector\Privatization\Rector\ClassMethod\PrivatizeLocalOnlyMethodRector;
+use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
+use Rector\Privatization\Rector\Property\PrivatizeLocalPropertyToPrivatePropertyRector;
 use Rector\Set\ValueObject\SetList;
-use Rector\SOLID\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
-use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -16,15 +26,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         [
             SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
             SetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
-            SetList::PHPSTAN,
-            SetList::PHPUNIT_CODE_QUALITY,
-            SetList::SOLID,
             SetList::DOCTRINE_CODE_QUALITY,
-            SetList::DEAD_CODE,
+            SetList::CODING_STYLE,
             SetList::CODE_QUALITY,
+            SetList::CODE_QUALITY_STRICT,
+            SetList::DEAD_CODE,
+            SetList::PRIVATIZATION,
+            SetList::NAMING,
+            SetList::PHPUNIT_CODE_QUALITY,
+            SetList::SYMFONY_AUTOWIRE,
             SetList::PHP_70,
             SetList::PHP_71,
             SetList::PHP_72,
+            SetList::EARLY_RETURN,
         ]
     );
     $parameters->set(
@@ -33,6 +47,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             FinalizeClassesWithoutChildrenRector::class,
             ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
             AddSeeTestAnnotationRector::class,
+            RepeatedLiteralToClassConstantRector::class,
+            PrivatizeLocalOnlyMethodRector::class,
+            RenameParamToMatchTypeRector::class,
+            RenameVariableToMatchMethodCallReturnTypeRector::class,
+            EncapsedStringsToSprintfRector::class,
+            PrivatizeLocalPropertyToPrivatePropertyRector::class,
+            ChangeOrIfReturnToEarlyReturnRector::class,
+            PrivatizeLocalGetterToPropertyRector::class,
+            ChangeAndIfToEarlyReturnRector::class,
+            VarConstantCommentRector::class,
         ]
     );
     $parameters->set(
