9b1a2526e066becaa15d1eef7336ff0cf94a834d
Zing <zingimmick@outlook.com>
2021-02-14 22:17:43 +0800

    Update Subscribable.php

--- src/Concerns/Subscribable.php
+++ src/Concerns/Subscribable.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace LaravelInteraction\Subscribe\Concerns;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
