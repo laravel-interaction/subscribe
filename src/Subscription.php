f4bb2fd1f40c080593fb42e9e6552f7b687f25dd
zingimmick <zingimmick@outlook.com>
2021-03-17 10:26:20 +0000

    Revert "Refactor Interaction (#7)" (#8)
    This reverts commit 3923fdc8257d8ab332640bfbe319cbd495eef8ca.

--- src/Subscription.php
+++ src/Subscription.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace LaravelInteraction\Subscribe;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Str;
 use LaravelInteraction\Subscribe\Events\Subscribed;
 use LaravelInteraction\Subscribe\Events\Unsubscribed;
-use LaravelInteraction\Support\InteractionList;
-use LaravelInteraction\Support\Models\Interaction;
 
 /**
  * @property \Illuminate\Database\Eloquent\Model $user
@@ -20,18 +21,50 @@ use LaravelInteraction\Support\Models\Interaction;
  * @method static \LaravelInteraction\Subscribe\Subscription|\Illuminate\Database\Eloquent\Builder withType(string $type)
  * @method static \LaravelInteraction\Subscribe\Subscription|\Illuminate\Database\Eloquent\Builder query()
  */
-class Subscription extends Interaction
+class Subscription extends MorphPivot
 {
-    protected $interaction = InteractionList::SUBSCRIBE;
+    protected function uuids(): bool
+    {
+        return (bool) config('subscribe.uuids');
+    }
 
-    protected $tableNameKey = 'subscriptions';
+    public function getIncrementing(): bool
+    {
+        return $this->uuids() ? true : parent::getIncrementing();
+    }
 
-    protected $morphTypeName = 'subscribable';
+    public function getKeyName(): string
+    {
+        return $this->uuids() ? 'uuid' : parent::getKeyName();
+    }
+
+    public function getKeyType(): string
+    {
+        return $this->uuids() ? 'string' : parent::getKeyType();
+    }
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(
+            function (self $like): void {
+                if ($like->uuids()) {
+                    $like->{$like->getKeyName()} = Str::orderedUuid();
+                }
+            }
+        );
+    }
 
     protected $dispatchesEvents = [
         'created' => Subscribed::class,
         'deleted' => Unsubscribed::class,
     ];
+
+    public function getTable()
+    {
+        return config('subscribe.table_names.subscriptions') ?: parent::getTable();
+    }
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\MorphTo
@@ -39,6 +72,14 @@ class Subscription extends Interaction
     public function subscribable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(config('subscribe.models.user'), config('subscribe.column_names.user_foreign_key'));
     }
 
     /**
@@ -57,5 +98,16 @@ class Subscription extends Interaction
     public function isSubscribedTo(Model $object): bool
     {
         return $object->is($this->subscribable);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $type
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithType(Builder $query, string $type): Builder
+    {
+        return $query->where('subscribable_type', app($type)->getMorphClass());
     }
 }
