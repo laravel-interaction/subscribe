9698cf998f6affc75f99e4e93a27a87a153b4fec
Zing <zingimmick@outlook.com>
2021-03-17 17:20:13 +0800

    Refactor ServiceProvider (#6)

--- src/SubscribeServiceProvider.php
+++ src/SubscribeServiceProvider.php
@@ -4,48 +4,10 @@ declare(strict_types=1);
 
 namespace LaravelInteraction\Subscribe;
 
-use Illuminate\Support\ServiceProvider;
+use LaravelInteraction\Support\InteractionList;
+use LaravelInteraction\Support\InteractionServiceProvider;
 
-class SubscribeServiceProvider extends ServiceProvider
+class SubscribeServiceProvider extends InteractionServiceProvider
 {
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    $this->getConfigPath() => config_path('subscribe.php'),
-                ],
-                'subscribe-config'
-            );
-            $this->publishes(
-                [
-                    $this->getMigrationsPath() => database_path('migrations'),
-                ],
-                'subscribe-migrations'
-            );
-            if ($this->shouldLoadMigrations()) {
-                $this->loadMigrationsFrom($this->getMigrationsPath());
-            }
-        }
-    }
-
-    public function register(): void
-    {
-        $this->mergeConfigFrom($this->getConfigPath(), 'subscribe');
-    }
-
-    protected function getConfigPath(): string
-    {
-        return __DIR__ . '/../config/subscribe.php';
-    }
-
-    protected function getMigrationsPath(): string
-    {
-        return __DIR__ . '/../migrations';
-    }
-
-    private function shouldLoadMigrations(): bool
-    {
-        return (bool) config('subscribe.load_migrations');
-    }
+    protected $interaction = InteractionList::SUBSCRIBE;
 }
